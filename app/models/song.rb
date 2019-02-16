c542b1ef8324a6aea7cc96a99dc06556ae6e6967
George  Pianka <gpianka1@gmail.com>
2019-02-16 07:36:53 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-     = Artist.find_or_create_by(name: name)
+    self.artist = Artist.find_or_create_by(name: name)
   
   end
 end
